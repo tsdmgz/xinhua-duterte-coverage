d6def33541ecdd088289cdb9279da6550ddbc569
tsdmgz <zeus@dmgznet.com>
2016-11-22 15:44:40 +0800

    Write article text extraction script
    new file:   article-extract-text.py

---
+++ article-extract-text.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+#Takes article body and title
+
+import argparse
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+
+parser = argparse.ArgumentParser(description="HTML file to use")
+parser.add_argument('-f', dest = 'file')
+args = parser.parse_args()
+html_doc = open(format(args.file))
+soup = BeautifulSoup(html_doc, "html.parser")
+div_content = soup.find_all(id='content')
+
+def check_style():
+	"Checks which title style is the article"
+	has_ftitle = soup.find_all(class_='f-title')
+	has_hei22 = soup.find_all(class_='hei22')
+	if has_ftitle:
+		return has_ftitle
+	elif has_hei22:
+		return has_hei22
+	
+title_style = check_style()
+
+for article_title_content in title_style:
+	print(article_title_content.get_text())
+
+print()
+
+for article_content in div_content:
+	for article_content_text in article_content.find_all('p'):
+		print(article_content_text.get_text())
+
+# vim: smartindent breakindent tw=80
