129ec7e548e7216cd07c9072d79a00287cab8259
tsdmgz <zeus@dmgznet.com>
2016-11-24 22:49:03 +0800

    Add classes to get_article_date()
    modified:   article-extract-text.py

--- article-extract-text.py
+++ article-extract-text.py
@@ -31,41 +31,40 @@ def get_div_content():
 
 def get_article_title():
 	"Checks which title style is the article"
-	has_ftitle = soup.find_all(class_='f-title')
-	has_hei22 = soup.find_all(class_='hei22')
-	has_bltitle = soup.find_all(id='bltitle')
-	has_capital_Title = soup.find_all(id='Title')
-	has_whtitle = soup.find_all(id='whtitle')
-	if has_ftitle:
-		return has_ftitle
-	elif has_hei22:
-		return has_hei22
-	elif has_bltitle:
-		return has_bltitle
-	elif has_capital_Title:
-		return has_capital_Title
-	elif has_whtitle:
-		return has_whtitle
+	is_class_ftitle = soup.find_all(class_='f-title')
+	is_class_hei22 = soup.find_all(class_='hei22')
+	is_id_bltitle = soup.find_all(id='bltitle')
+	is_id_capital_Title = soup.find_all(id='Title')
+	is_id_whtitle = soup.find_all(id='whtitle')
+	if is_class_ftitle:
+		return is_class_ftitle
+	elif is_class_hei22:
+		return is_class_hei22
+	elif is_id_bltitle:
+		return is_id_bltitle
+	elif is_id_capital_Title:
+		return is_id_capital_Title
+	elif is_id_whtitle:
+		return is_id_whtitle
 	else:
 		return None
 
 def get_article_date():
 	"Get article's date and time"
 	date_pattern = re.compile("\d+\-\d+-\d+")
-	has_sj = soup.find_all(class_='sj')
-	has_pubtime = soup.find_all(id='pubtime')
-	has_lanx12 = soup.find_all(class_='lanx12')
-	if has_sj:
-		has_date = has_sj
-	elif has_pubtime:
-		has_date = has_pubtime
-	elif has_lanx12:
-		has_date = has_lanx12
+	is_class_sj = soup.find_all(class_='sj')
+	is_id_pubtime = soup.find_all(id='pubtime')
+	is_class_lanx12 = soup.find_all(class_='lanx12')
+	if is_class_sj:
+		has_date = is_class_sj
+	elif is_id_pubtime:
+		has_date = is_id_pubtime
+	elif is_class_lanx12:
+		has_date = is_class_lanx12
 	date_obj = has_date[0].get_text()
 	date_re = date_pattern.search(date_obj)
 	stripped_date = date_re.group()
 	return stripped_date
-	
 
 print(get_article_date())
 print()
